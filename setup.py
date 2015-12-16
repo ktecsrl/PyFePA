292337e1e0d2b787b593210d6ef5b02e0cd6a0a6
Luigi Di Naro <gigidn@gmail.com>
2015-12-16 16:10:42 +0100

    [FIX] Root tag case problem on SIAMM validation

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ files = ["xsd/*.xsd"]
 setup(
   name = 'PyFePA',
   packages = ['PyFePA'],
-  version = '1.0.1',
+  version = '1.0.2',
   description = 'Python object of italian FatturaPA, serialize, deserialize and verify',
   author = 'Luigi Di Naro',
   author_email = 'Luigi.DiNaro@ktec.it',
